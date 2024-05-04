e03363706351ec7ec79119c62c8b62af44d5f62a
Achillia MFG <gxcs.sexy@gmail.com>
2024-05-04 18:27:48 +0900

    2 commit

---
+++ MyBingo/MyBingoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBingoApp.swift
+//  MyBingo
+//
+//  Created by Guan Xin on 2024/05/04.
+//
+
+import SwiftUI
+
+@main
+struct MyBingoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
